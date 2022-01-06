a2d4c4c6802bdd8fc0d4177b972b43c2959619e9
Ahshan Manzar <=AM00630805@techmahindra.com>
2022-01-06 23:22:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         !! HAPPY NEW YEAR 2022 !!
         </p>
         <a
           className="App-link"
